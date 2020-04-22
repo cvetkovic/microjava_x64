19f4a092484338adc99f64a307eb02b431f094dc
Lazar M. Cvetković <l.cvetkovic.997@gmail.com>
2020-04-22 20:44:09 +0200

    bash script

--- test/unix_test.sh
+++ test/unix_test.sh
@@ -1,4 +1,8 @@
 #!/usr/bin/env bash
 
-gcc -o /mnt/c/Users/cl160127d/IdeaProjects/microjava_x64/test/program -static /mnt/c/Users/cl160127d/IdeaProjects/microjava_x64/test/program.s -fPIC
-/mnt/c/Users/cl160127d/IdeaProjects/microjava_x64/test/program
+OUTPUT="/mnt/c/Users/cl160127d/IdeaProjects/microjava_x64/test/program"
+ASSEMBLY_INPUT="/mnt/c/Users/cl160127d/IdeaProjects/microjava_x64/test/program.s"
+
+
+gcc -o $OUTPUT -static $ASSEMBLY_INPUT -fPIC
+eval "$OUTPUT"
